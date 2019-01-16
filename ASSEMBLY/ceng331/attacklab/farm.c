66e26373b2465f336a935c63996689e72ed9313d
emrahkosen <36587182+emrahkosen@users.noreply.github.com>
2019-01-16 16:02:19 +0300

    Add files via upload

---
+++ ASSEMBLY/ceng331/attacklab/farm.c
@@ -0,0 +1,151 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+void setval_249(unsigned *p)
+{
+    *p = 3733538918U;
+}
+
+unsigned getval_142()
+{
+    return 3286141257U;
+}
+
+unsigned addval_131(unsigned x)
+{
+    return x + 3284830536U;
+}
+
+void setval_426(unsigned *p)
+{
+    *p = 2438465688U;
+}
+
+unsigned getval_100()
+{
+    return 3616098534U;
+}
+
+void setval_447(unsigned *p)
+{
+    *p = 2463598920U;
+}
+
+void setval_481(unsigned *p)
+{
+    *p = 3247000692U;
+}
+
+void setval_140(unsigned *p)
+{
+    *p = 3286141256U;
+}
+
+void setval_448(unsigned *p)
+{
+    *p = 2428733768U;
+}
+
+void setval_110(unsigned *p)
+{
+    *p = 3285057864U;
+}
+
+unsigned addval_322(unsigned x)
+{
+    return x + 2429192520U;
+}
+
+unsigned addval_365(unsigned x)
+{
+    return x + 3285057866U;
+}
+
+void setval_414(unsigned *p)
+{
+    *p = 2462746952U;
+}
+
+void setval_285(unsigned *p)
+{
+    *p = 3284371784U;
+}
+
+unsigned addval_296(unsigned x)
+{
+    return x + 3285682504U;
+}
+
+unsigned getval_207()
+{
+    return 2446197064U;
+}
+
+unsigned addval_176(unsigned x)
+{
+    return x + 3285057864U;
+}
+
+void setval_356(unsigned *p)
+{
+    *p = 2429193544U;
+}
+
+void setval_149(unsigned *p)
+{
+    *p = 3281016885U;
+}
+
+unsigned addval_211(unsigned x)
+{
+    return x + 3284240712U;
+}
+
+unsigned getval_129()
+{
+    return 3281016957U;
+}
+
+void setval_205(unsigned *p)
+{
+    *p = 3733800969U;
+}
+
+void setval_155(unsigned *p)
+{
+    *p = 2428733512U;
+}
+
+void setval_469(unsigned *p)
+{
+    *p = 2445510984U;
+}
+
+unsigned addval_284(unsigned x)
+{
+    return x + 3268905288U;
+}
+
+void setval_107(unsigned *p)
+{
+    *p = 3284240712U;
+}
+
+unsigned addval_375(unsigned x)
+{
+    return x + 2425393496U;
+}
+
+unsigned addval_412(unsigned x)
+{
+    return x + 3284240456U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
